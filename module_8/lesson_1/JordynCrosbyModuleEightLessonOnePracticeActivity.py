9003af5a582b690a4c7c95298ac5e43c096e8b2e
Omar Crosby <omar.crosby@gmail.com>
2020-12-07 19:47:52 -0500

    I'm done

--- module_8/lesson_1/JordynCrosbyModuleEightLessonOnePracticeActivity.py
+++ module_8/lesson_1/JordynCrosbyModuleEightLessonOnePracticeActivity.py
@@ -1,4 +1,17 @@
 # [ ] Import the math module and use an appropriate function to find the greatest common divisor of 16 and 28
 
-import math 
-math.gcd(16,28)
+import math
+
+divisor = math.gcd(16,28)
+
+print(divisor)
+
+value1 = input('Enter an integer number: ')
+value2 = input('Enter another integer number: ')
+
+value1 = int(value1)
+value2 = int(value2)
+
+divisor = math.gcd(value1, value2)
+
+print("The greatest common divisor of {0} and {1} is {2}".format(value1, value2, divisor))
