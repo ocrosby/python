426f4642e6f1792c6f48b2b8234f07fe23024ab2
Jordyn Crosby <jordyncrosby@Jordyns-MacBook-Pro.local>
2020-12-10 10:39:15 -0500

    micellanous changes

--- module_8/lesson_1/JordynCrosbyModuleEightLessonOnePracticeActivity.py
+++ module_8/lesson_1/JordynCrosbyModuleEightLessonOnePracticeActivity.py
@@ -1,4 +1,14 @@
 # [ ] Import the math module and use an appropriate function to find the greatest common divisor of 16 and 28
 
 import math 
-math.gcd(16,28)
+divisor = math.gcd(16,28)
+print(divisor)
+
+# [ ] Prompt the user to input 2 positive integers then print their greatest common divisor
+int1 = input("Input a positive integer: ")
+int1 = int(int1)
+int2 = input("Input another positive integer:  ")
+int2 = int(int2)
+
+user_gcd = math.gcd(int1,int2)
+print("The greatest common divisor of {0} and {1} is {2}".format(int1,int2,user_gcd))
